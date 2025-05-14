1216a864512cc5c83e5960060d07e504895094bf
skyedgedigital <skyedgedigital01@gmail.com>
2025-05-14 11:52:04 +0530

    Update calcPrice.ts (removed the special case for 'shift')

--- utils/calcPrice.ts
+++ utils/calcPrice.ts
@@ -2,10 +2,10 @@ const calcPrice = (quantity:number,price:number,unit:string) => {
     // if(unit === "minutes"){
     //     return quantity*( price / 60.0 );
     // }
-    if(unit === "shift"){
-        return 1.00 * price;
-    }
-    else if(unit==="ot"){
+    // if(unit === "shift"){
+    //     return 1.00 * price;
+    // }
+     if(unit==="ot"){
         return 0.5 * price * quantity;
     }
     return quantity*price;
