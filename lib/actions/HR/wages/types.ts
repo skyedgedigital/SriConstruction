14e00977d08df85ccc69f32c622eb7530e1a1033
paro-418 <vermapraween6543@gmail.com>
2025-12-17 13:50:04 +0530

    added wages missing types

---
+++ lib/actions/HR/wages/types.ts
@@ -0,0 +1,11 @@
+export type YearlyWageInfo = {
+  year: number;
+  totalAttendance: number;
+  totalNetAmountPaid: number;
+  status: boolean;
+  grossAmountYearly: number;
+  EL: number;
+  CL: number;
+  FL: number;
+  leave: number;
+};
